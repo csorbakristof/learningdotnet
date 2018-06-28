19e2eb99e746a3e7ecb77e1d2a32e85842313b7d
Kristof <kristof@aut.bme.hu>
2018-06-28 18:38:18 +0200

    Creating shape with ColorPicker works.

--- FirstUwpApp/View/FirstPage.xaml.cs
+++ FirstUwpApp/View/FirstPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +28,29 @@ namespace FirstUwpApp.View
         public FirstPage()
         {
             this.InitializeComponent();
+        }
+
+        private void CreateShapeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var s = CreateShape();
+            s.Stroke = new SolidColorBrush(Windows.UI.Colors.Blue);
+            s.Fill = new SolidColorBrush(this.Color.Color);
+            this.Canvas.Children.Add(s);
+            Canvas.SetLeft(s, 10);
+            Canvas.SetTop(s, 10);
+        }
+
+        private Shape CreateShape()
+        {
+            var type = this.Type.SelectedValue as ComboBoxItem;
+            switch (type.Content)
+            {
+                case "Polygon":
+                    return new Rectangle() { Width = 50, Height = 50 };
+                case "Circle":
+                    return new Ellipse() { Width = 50, Height = 50 };
+            }
+            return null;
         }
     }
 }
