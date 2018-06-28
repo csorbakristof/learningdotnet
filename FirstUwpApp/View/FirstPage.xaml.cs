fd1eff459cffdd292fcb773a5e4f7c26f88323ec
Kristof <kristof@aut.bme.hu>
2018-06-28 19:36:01 +0200

    Using click to place new shapes

--- FirstUwpApp/View/FirstPage.xaml.cs
+++ FirstUwpApp/View/FirstPage.xaml.cs
@@ -12,23 +12,33 @@ namespace FirstUwpApp.View
             this.InitializeComponent();
         }
 
-        private void CreateShapeButton_Click(object sender, RoutedEventArgs e)
+        private void Canvas_PointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            var p = e.GetCurrentPoint(this.Canvas);
+            CreateShapeAtLocation(p.Position.X, p.Position.Y);
+        }
+
+        private void CreateShapeAtLocation(double x, double y)
         {
             var s = CreateShape();
-            s.Stroke = new SolidColorBrush(Windows.UI.Colors.Blue);
-            s.Fill = new SolidColorBrush(this.Color.Color);
             this.Canvas.Children.Add(s);
-            Canvas.SetLeft(s, 10);
-            Canvas.SetTop(s, 10);
+            Canvas.SetLeft(s, x);
+            Canvas.SetTop(s, y);
         }
 
         private Shape CreateShape()
         {
+            Shape s = null;
             if (this.ShapeIsCircle.IsChecked ?? false)
-                return new Ellipse() { Width = WidthSlider.Value, Height = HeightSlider.Value };
+                s = new Ellipse() { Width = WidthSlider.Value, Height = HeightSlider.Value };
             else if (this.ShapeIsRectangle.IsChecked ?? false)
-                return new Rectangle() { Width = WidthSlider.Value, Height = HeightSlider.Value };
-            return null;
+                s = new Rectangle() { Width = WidthSlider.Value, Height = HeightSlider.Value };
+            else
+                return null;
+            s.Stroke = new SolidColorBrush(Windows.UI.Colors.Blue);
+            s.Fill = new SolidColorBrush(this.Color.Color);
+            return s;
         }
+
     }
 }
