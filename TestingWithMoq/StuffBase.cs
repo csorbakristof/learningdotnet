caefcaa4e883070c139eef78acb200eca1dfaa23
Kristof <kristof@aut.bme.hu>
2018-06-30 19:25:52 +0200

    Tests using Moq added. (Turkmite was removed, no good place for mocks.)

---
+++ TestingWithMoq/StuffBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWithMoq
+{
+    // Base class for Stuff (to be tested)
+    public class StuffBase
+    {
+        private ISource src;
+        private ICommand cmd;
+        public StuffBase(ISource src, ICommand cmd)
+        {
+            this.src = src;
+            this.cmd = cmd;
+        }
+
+        public virtual void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public void ExecuteCommandIfSourceIsNonzero()
+        {
+            if (src.GetValue() != 0)
+                cmd.Execute();
+        }
+
+        public int GetValueFromInternalSource()
+        {
+            return src.GetValue();
+        }
+    }
+}
---
+++ TestingWithMoq/StuffTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingWithMoq
+{
+    [TestClass]
+    public class StuffTests
+    {
+        [TestMethod]
+        public void StuffBaseInstantiates()
+        {
+            var sb = new StuffBase(null,null);
+        }
+
+        [TestMethod]
+        public void StuffBaseUsesSource()
+        {
+            var srcMock = new Moq.Mock<ISource>();
+            srcMock.Setup(i => i.GetValue()).Returns(3);
+            var sb = new StuffBase(srcMock.Object, null);
+            Assert.AreEqual(3, sb.GetValueFromInternalSource());
+        }
+
+        [TestMethod]
+        public void StuffCallsCommand()
+        {
+            var mockCmd = new Moq.Mock<ICommand>();
+            var mockSrc = new Moq.Mock<ISource>();
+            mockSrc.Setup(i => i.GetValue()).Returns(1);
+            var sb = new StuffBase(mockSrc.Object, mockCmd.Object);
+            sb.ExecuteCommandIfSourceIsNonzero();
+            mockCmd.Verify(i => i.Execute());
+        }
+
+        [TestMethod]
+        public void StuffCallsVisitor()
+        {
+            var mockVisitor = new Moq.Mock<IVisitor>();
+            var sb = new StuffBase(null,null);
+            sb.Accept(mockVisitor.Object);
+            mockVisitor.Verify(i => i.Visit(sb));
+        }
+    }
+}
