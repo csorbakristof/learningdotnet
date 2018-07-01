e08195708ab44e67271be04ec29aa384c6b93d19
Kristof <kristof@aut.bme.hu>
2018-07-01 13:33:59 +0200

    ExcelHelper and its tests projects added

---
+++ ExcelHelperTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExcelHelperTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExcelHelperTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2b15662-b388-42b7-99ed-3e77e51e4738")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExcelHelperTest/StudentBasicTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExcelHelper;
+
+namespace ExcelHelperTest
+{
+    /// <summary>
+    /// Summary description for StudentBasicTests
+    /// </summary>
+    [TestClass]
+    public class StudentBasicTests
+    {
+        public StudentBasicTests()
+        {
+        }
+
+        [TestMethod]
+        public void Istantiation()
+        {
+            var s = new Student();
+        }
+    }
+}
