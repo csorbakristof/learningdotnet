0cd833c75c516facafb0ddd239bbc51370fb9dae
Kristof <kristof@aut.bme.hu>
2018-07-01 15:23:38 +0200

    Student handling complete with TDD

--- ExcelHelper/Student.cs
+++ ExcelHelper/Student.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExcelHelper
 {
     public class Student
     {
+        private string GradedSubject = null;
+        private int? Grade = null;
+        private List<string> Attendances = new List<string>();
+
+        public static readonly string GradingOK = "OK";
+        public static readonly string SingleMismatchingAttendanceOK = "OK, subject mismatch: attended {0}, graded {1}";
+        public static string MultipleAttendedSubjectsThisOneUngradedOK
+            = "OK, multiple attendances, graded {0} instead of this {1}.";
+        public static string Ungraded = "Ungraded, advisor: {0}";
+
+        public string Advisor { get; set; }
+
+        public void SetGrade(string subject, int grade)
+        {
+            GradedSubject = subject;
+            Grade = grade;
+        }
+
+        public void AddAttendance(string subject)
+        {
+            Attendances.Add(subject);
+        }
+
+        public bool IsAttending(string subject)
+        {
+            return Attendances.Contains(subject);
+        }
+
+        public int? GetGrade(string subject)
+        {
+            GetGradingSituation(subject);   // Remains for exception checking
+            return Grade;
+        }
+
+        public string GetGradingSituation(string subject)
+        {
+            if (!Attendances.Contains(subject))
+                throw new ArgumentException("Should not ask grading for unattended subject.");
+
+            if (Grade == null)
+                return string.Format(Ungraded, Advisor);
+
+            // Now we are sure they attended the asked subject.
+            if (Attendances.Count == 1)
+            {
+                return (subject == GradedSubject)
+                    ? GradingOK
+                    : string.Format(SingleMismatchingAttendanceOK, subject, GradedSubject);
+            }
+            else // Multiple attendances
+            {
+                return (GradedSubject == subject)
+                    ? GradingOK
+                    : string.Format(MultipleAttendedSubjectsThisOneUngradedOK,
+                        GradedSubject, subject);
+            }
+        }
     }
 }
--- ExcelHelperTest/StudentBasicTests.cs
+++ ExcelHelperTest/StudentBasicTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ExcelHelper;
 
@@ -12,14 +10,139 @@ namespace ExcelHelperTest
     [TestClass]
     public class StudentBasicTests
     {
+        private Student s = new Student();
         public StudentBasicTests()
         {
         }
 
+        const string SubjectCode = "VIAUA000";
+        const string WrongSubjectCode = "ThereIsNoSuchCode";
+        const string WrongSubjectCode2 = "ThereIsNoSuchCode2";
+
         [TestMethod]
-        public void Istantiation()
+        public void CanStoreGrade()
         {
-            var s = new Student();
+            s.SetGrade(SubjectCode, 5);
+            s.AddAttendance(SubjectCode);
+            Assert.AreEqual(5, s.GetGrade(SubjectCode));
+            expectGradingException(s, WrongSubjectCode);
         }
+
+        [TestMethod]
+        public void CanStoreAttendance()
+        {
+            s.AddAttendance(SubjectCode);
+            Assert.IsTrue(s.IsAttending(SubjectCode));
+            Assert.IsFalse(s.IsAttending(WrongSubjectCode));
+        }
+
+        #region Queries for grade with various attendance and graded subject patterns
+        [TestMethod]
+        public void NoAttendedSubject_AskedGradedSubject_Rejects()
+        {
+            s.SetGrade(SubjectCode, 5);
+            expectGradingException(s, SubjectCode);
+        }
+
+        [TestMethod]
+        public void NoAttendedSubject_AskedDifferentSubject_Rejects()
+        {
+            s.SetGrade(SubjectCode, 5);
+            expectGradingException(s, WrongSubjectCode);
+        }
+
+        [TestMethod]
+        public void SingleDifferentAttendedSubject_AskedAttendedSubject_ReturnsGrade()
+        {
+            s.SetGrade(SubjectCode, 5);
+            s.AddAttendance(WrongSubjectCode);
+            Assert.AreEqual(string.Format(
+                Student.SingleMismatchingAttendanceOK, WrongSubjectCode, SubjectCode),
+                s.GetGradingSituation(WrongSubjectCode));
+            Assert.AreEqual(5, s.GetGrade(WrongSubjectCode));
+        }
+
+        [TestMethod]
+        public void SingleDifferentAttendedSubject_AskedUnattendedSubject_Rejects()
+        {
+            s.SetGrade(SubjectCode, 5);
+            s.AddAttendance(WrongSubjectCode);
+            expectGradingException(s, WrongSubjectCode2);
+        }
+
+        [TestMethod]
+        public void SingleDifferentAttendedSubject_AskedUnattendedButGradedSubject_Rejects()
+        {
+            s.SetGrade(SubjectCode, 5);
+            s.AddAttendance(WrongSubjectCode);
+            expectGradingException(s, SubjectCode);
+        }
+
+        [TestMethod]
+        public void MultipleAttendedSubject_AskedAttendedGradedSubject_ReturnsGrade()
+        {
+            s.SetGrade(SubjectCode, 5);
+            s.AddAttendance(SubjectCode);
+            s.AddAttendance(WrongSubjectCode);
+            Assert.AreEqual(Student.GradingOK, s.GetGradingSituation(SubjectCode));
+            Assert.AreEqual(5, s.GetGrade(SubjectCode));
+        }
+
+        [TestMethod]
+        public void MultipleAttendedSubject_AskedUnattendedGradedSubject_Rejects()
+        {
+            s.SetGrade(SubjectCode, 5);
+            s.AddAttendance(WrongSubjectCode);
+            s.AddAttendance(WrongSubjectCode2);
+            expectGradingException(s, SubjectCode);
+        }
+
+        [TestMethod]
+        public void MultipleAttendedSubject_AskedAttendedUngradedSubject_ReturnsGrade()
+        {
+            s.SetGrade(SubjectCode, 5);
+            s.AddAttendance(SubjectCode);
+            s.AddAttendance(WrongSubjectCode);
+            Assert.AreEqual(string.Format(
+                Student.MultipleAttendedSubjectsThisOneUngradedOK, SubjectCode, WrongSubjectCode),
+                s.GetGradingSituation(WrongSubjectCode));
+            Assert.AreEqual(5, s.GetGrade(WrongSubjectCode));
+        }
+
+        private void expectGradingException(Student s, string subject)
+        {
+            bool eThrown = false;
+            try
+            {
+                s.GetGrade(subject);
+            }
+            catch (ArgumentException)
+            {
+                eThrown = true;
+            }
+            Assert.IsTrue(eThrown);
+        }
+        #endregion
+
+        #region Setting advisor, query ungraded student
+        [TestMethod]
+        public void AttendingButUngraded_GradingSituationReturnsAdvisorName()
+        {
+            s.AddAttendance(SubjectCode);
+            s.Advisor = "Minta Mókus";
+            Assert.AreEqual(
+                string.Format(Student.Ungraded,s.Advisor),
+                s.GetGradingSituation(SubjectCode));
+            Assert.AreEqual(null, s.GetGrade(SubjectCode));
+        }
+
+        [TestMethod]
+        public void UnattendingAndUngraded_GradingThrowsException()
+        {
+            s.AddAttendance(SubjectCode);
+            expectGradingException(s, WrongSubjectCode);
+        }
+        #endregion
+
     }
 }
