6c595e3f7f90036ef9ebd384c015315a8bdbdba0
Kristof <kristof@aut.bme.hu>
2018-08-16 14:15:38 +0200

    Test refactor, merging emails for the same presenter.

--- PeerReviewResultDistributionHelperTests/Review.cs
+++ PeerReviewResultDistributionHelperTests/Review.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Windows.ApplicationModel.Email;
 
 namespace PeerReviewResultDistributionHelperTests
@@ -31,6 +34,20 @@ namespace PeerReviewResultDistributionHelperTests
                 + $"Értékelő hallgató: {s.StudentName}({s.StudentNeptunCode}, {s.StudentEmail}) írta:\n\n"
                 + $"Összesített pontszám: {OverallScore}\n\n"
                 + $"{Text}\n\n";
+            return e;
+        }
+
+        internal static EmailMessage GetCollectedPresenterEmail(string presenterEmail, List<Review> allReviews, SupervisionLookupBase supervisionLookup)
+        {
+            var reviews = allReviews.Where(r => r.PresenterEmail == presenterEmail);
+            var e = new EmailMessage();
+            e.To.Add(new EmailRecipient(presenterEmail));
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Kedves Hallgatónk!\n\n" +
+                "A beszámolódhoz az alábbi értékelések gyűltek össze:\n\n");
+            foreach(var r in reviews)
+                sb.Append($"----- Értékelés - összesített pontszám: {r.OverallScore}\n\n{r.Text}\n\n");
+            e.Body = sb.ToString();
             return e;
         }
     }
--- PeerReviewResultDistributionHelperTests/ReviewTests.cs
+++ PeerReviewResultDistributionHelperTests/ReviewTests.cs
@@ -8,41 +8,76 @@ namespace PeerReviewResultDistributionHelperTests
     [TestClass]
     public partial class BasicReviewTests
     {
-        [TestMethod]
-        public void CanSetProperties()
+        class TestCase
         {
-            CreateTestReview();
+            public string ReviewText;
+            public int ReviewOverallScore;
+            public string ReviewerName;
+            public string ReviewerNeptunCode;
+            public string PresenterEmail;
+            public string AdvisorName;
+            public string AdvisorEmail;
         }
 
-        const string ReviewText = "Arbitrary review message";
-        const int ReviewOverallScore = 5;
-        const string ReviewerName = "Ertekelo Elek";
-        const string ReviewerNeptunCode = "ABC123";
-        const string PresenterEmail = "presenter@example.com";
-        const string AdvisorName = "Konzulens Klára";
-        const string AdvisorEmail = "advisor@example.com";
+        private TestCase[] testCases;
 
-        private Review CreateTestReview()
+        public BasicReviewTests()
+        {
+            // Note:
+            //  all 3 reviewers are different
+            //  case 1 has same presenter as case 0
+            //  case 2 has same advisor as case 0
+
+            testCases = new TestCase[3];
+            testCases[0] = new TestCase()
+            {
+                ReviewText = "Arbitrary review message 1",
+                ReviewOverallScore = 1,
+                ReviewerName = "Ertekelo Elek",
+                ReviewerNeptunCode = "ABC123",
+                PresenterEmail = "presenter@example.com",
+                AdvisorName = "Konzulens Klára",
+                AdvisorEmail = "advisor@example.com"
+            };
+            testCases[1] = new TestCase()
+            {
+                ReviewText = "Arbitrary review message 2",
+                ReviewOverallScore = 2,
+                ReviewerName = "Ertekelo Ferenc",
+                ReviewerNeptunCode = "DEF456",
+                PresenterEmail = testCases[0].PresenterEmail,
+                AdvisorName = "Konzulens Kázmér",
+                AdvisorEmail = "advisor@example.com"
+            };
+            testCases[2] = new TestCase()
+            {
+                ReviewText = "Arbitrary review message 3",
+                ReviewOverallScore = 3,
+                ReviewerName = "Dummy Dénes",
+                ReviewerNeptunCode = "DDD777",
+                PresenterEmail = "otherPresenter@example.com",
+                AdvisorName = testCases[0].AdvisorName,
+                AdvisorEmail = testCases[0].AdvisorEmail
+            };
+        }
+
+        private Review CreateTestReview(TestCase testCase)
         {
             var review = new Review();
-            review.Text = ReviewText;
-            review.OverallScore = ReviewOverallScore;
-            review.ReviewerNeptunCode = ReviewerNeptunCode;
-            review.PresenterEmail = PresenterEmail;
+            review.Text = testCase.ReviewText;
+            review.OverallScore = testCase.ReviewOverallScore;
+            review.ReviewerNeptunCode = testCase.ReviewerNeptunCode;
+            review.PresenterEmail = testCase.PresenterEmail;
             return review;
         }
 
         [TestMethod]
         public void GeneratesPresenterEmailCorrectly()
         {
-            var r = CreateTestReview();
+            var r = CreateTestReview(testCases[0]);
             var m = r.GetPresenterEmail();
-            Assert.AreEqual(PresenterEmail, m.To[0].Address);
-            Assert.IsTrue(m.Body.Contains($"pontszám: {ReviewOverallScore}"));
-            Assert.IsTrue(m.Body.Contains(ReviewText));
-            Assert.IsFalse(m.Body.Contains(AdvisorName));
-            Assert.IsFalse(m.Body.Contains(ReviewerName));
-            Assert.IsFalse(m.Body.Contains(ReviewerNeptunCode));
+            Assert.AreEqual(testCases[0].PresenterEmail, m.To[0].Address);
+            AssertEmailBodyForPresenter(m, 0);
         }
 
 
@@ -50,54 +85,80 @@ namespace PeerReviewResultDistributionHelperTests
         public void SupervisionLookupTest()
         {
             var lookup = new SupervisionLookupBase();
-            var s = CreatePresetSupervision();
-            lookup.Add(ReviewerNeptunCode, s);
-            var result = lookup.GetSupervision(ReviewerNeptunCode);
+            var s = CreateTestSupervision(testCases[0]);
+            lookup.Add(testCases[0].ReviewerNeptunCode, s);
+            var result = lookup.GetSupervision(testCases[0].ReviewerNeptunCode);
             Assert.AreSame(s, result);
         }
 
         [TestMethod]
         public void GeneratesAdvisorEmailCorrectly()
         {
-            var r = CreateTestReview();
+            var r = CreateTestReview(testCases[0]);
 
             var lookup = new SupervisionLookupBase();
-            var s = CreatePresetSupervision();
-            lookup.Add(ReviewerNeptunCode, s);
+            var s = CreateTestSupervision(testCases[0]);
+            lookup.Add(testCases[0].ReviewerNeptunCode, s);
 
             var m = r.GetAdvisorEmail(lookup);
-            Assert.AreEqual(AdvisorEmail, m.To[0].Address);
-            Assert.IsFalse(m.Body.Contains(AdvisorEmail));
-            Assert.IsTrue(m.Body.Contains(ReviewText));
-            Assert.IsTrue(m.Body.Contains($"pontszám: {ReviewOverallScore}"));
-            Assert.IsTrue(m.Body.Contains(ReviewerName));
-            Assert.IsTrue(m.Body.Contains(ReviewerNeptunCode));
-            Assert.IsTrue(m.Body.Contains(AdvisorName));
+            Assert.AreEqual(testCases[0].AdvisorEmail, m.To[0].Address);
+            Assert.IsFalse(m.Body.Contains(testCases[0].AdvisorEmail));
+            Assert.IsTrue(m.Body.Contains(testCases[0].ReviewText));
+            Assert.IsTrue(m.Body.Contains($"pontszám: {testCases[0].ReviewOverallScore}"));
+            Assert.IsTrue(m.Body.Contains(testCases[0].ReviewerName));
+            Assert.IsTrue(m.Body.Contains(testCases[0].ReviewerNeptunCode));
+            Assert.IsTrue(m.Body.Contains(testCases[0].AdvisorName));
         }
 
-        Supervision CreatePresetSupervision()
-        {
-            return new Supervision() { StudentName = ReviewerName, StudentNeptunCode = ReviewerNeptunCode, AdvisorName = AdvisorName, AdvisorEmail = AdvisorEmail };
-        }
-
-        Supervision CreateDummySupervision()
+        Supervision CreateTestSupervision(TestCase testCase)
         {
             return new Supervision() {
-                StudentName = "DummyStudent",
-                StudentNeptunCode = "DEF456",
-                AdvisorName = "DummyAdvisor",
-                AdvisorEmail = "dummyAdvisor@example.com"
-            };
+                StudentName = testCase.ReviewerName,
+                StudentNeptunCode = testCase.ReviewerNeptunCode,
+                AdvisorName = testCase.AdvisorName,
+                AdvisorEmail = testCase.AdvisorEmail };
         }
-
 
         [TestMethod]
         public void SupervisionLookupBaseWithMultipleEntries()
         {
             var lookup = new SupervisionLookupBase();
-            lookup.Add(ReviewerNeptunCode, CreatePresetSupervision());
-            lookup.Add("DEF456", CreateDummySupervision());
-            Assert.AreEqual(AdvisorName, lookup.GetSupervision(ReviewerNeptunCode).AdvisorName);
+            lookup.Add(testCases[0].ReviewerNeptunCode, CreateTestSupervision(testCases[0]));
+            lookup.Add(testCases[1].ReviewerNeptunCode, CreateTestSupervision(testCases[1]));
+            Assert.AreEqual(testCases[0].AdvisorName, lookup.GetSupervision(testCases[0].ReviewerNeptunCode).AdvisorName);
+        }
+
+        [TestMethod]
+        public void MultipleReviewsForSamePresenter_EmailCollectsReviews()
+        {
+            (var reviews, var s) = CreateFullTestCase();
+
+            var mail = Review.GetCollectedPresenterEmail(testCases[0].PresenterEmail, reviews, s);
+            // Note: test cases 0 and 1 have same presenter
+            Assert.AreEqual(testCases[0].PresenterEmail, mail.To[0].Address);
+            foreach (int idx in new int[] { 0, 1 })
+                AssertEmailBodyForPresenter(mail, idx);
+        }
+
+        private (List<Review> reviews, SupervisionLookupBase s) CreateFullTestCase()
+        {
+            var reviews = new List<Review>();
+            var s = new SupervisionLookupBase();
+            foreach (var tc in testCases)
+            {
+                reviews.Add(CreateTestReview(tc));
+                s.Add(tc.ReviewerNeptunCode, CreateTestSupervision(tc));
+            }
+            return (reviews, s);
+        }
+
+        private void AssertEmailBodyForPresenter(Windows.ApplicationModel.Email.EmailMessage mail, int idx)
+        {
+            Assert.IsTrue(mail.Body.Contains($"pontszám: {testCases[idx].ReviewOverallScore}"));
+            Assert.IsTrue(mail.Body.Contains(testCases[idx].ReviewText));
+            Assert.IsFalse(mail.Body.Contains(testCases[idx].AdvisorName));
+            Assert.IsFalse(mail.Body.Contains(testCases[idx].ReviewerName));
+            Assert.IsFalse(mail.Body.Contains(testCases[idx].ReviewerNeptunCode));
         }
     }
 }
